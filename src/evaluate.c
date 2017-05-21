7d187ca4843352fb21da4bd622d20a1eb955927d
ChessMan3 <amult@ukr.net>
2017-05-21 16:56:32 +0300

    Simplification of lazy threshold
    Passed STC
    http://tests.stockfishchess.org/tests/view/587846c10ebc5915193f74ec
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 217236 W: 39041 L: 39254 D: 138941
    
    Passed LTC
    http://tests.stockfishchess.org/tests/view/587e157a0ebc5915193f76e7
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 52396 W: 6883 L: 6804 D: 38709
    
    This submitted version (using if (abs(mg + eg) > 1500) )
    seems more logical than the following other green simplification (using if (abs(mg)>1500))
    since it can happen than mg_value is > eg_value (about 20% of the time)
    and the submitted version seems stronger at LTC
    
    STC
    http://tests.stockfishchess.org/tests/view/5879702d0ebc5915193f7585
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 39958 W: 7315 L: 7227 D: 25416
    
    LTC
    http://tests.stockfishchess.org/tests/view/5879af3e0ebc5915193f7592
    LLR: 2.95 (-2.94,2.94) [-3.00,1.00]
    Total: 204322 W: 26529 L: 26648 D: 151145
    
    bench: 6406285

--- src/evaluate.c
+++ src/evaluate.c
@@ -177,7 +177,7 @@ static const int KingAttackWeights[8] = { 0, 0, 78, 56, 45, 11 };
 #define KnightCheck       924
 
 // Threshold for lazy evaluation
-const Value LazyEval = (1500);
+const Value LazyThreshold = (1500);
 
 // eval_init() initializes king and attack bitboards for a given color
 // adding pawn attacks. To be done at the beginning of the evaluation.
@@ -738,17 +738,6 @@ INLINE int evaluate_scale_factor(const Pos *pos, EvalInfo *ei, Value eg)
   return sf;
 }
 
-Value lazy_eval(Value mg, Value eg) {
-  
- if (mg > LazyEval && eg > LazyEval)
-    return  LazyEval + ((mg + eg) / 2 - LazyEval) / 4;
-   
- else if (mg < -LazyEval && eg < -LazyEval)
-    return -LazyEval + ((mg + eg) / 2 + LazyEval) / 4;
-  
- return VALUE_ZERO;
- }
-
 // evaluate() is the main evaluation function. It returns a static evaluation
 // of the position from the point of view of the side to move.
 
@@ -757,6 +746,7 @@ Value evaluate(const Pos *pos)
   assert(!pos_checkers());
 
   Score mobility[2] = { SCORE_ZERO, SCORE_ZERO };
+  Value v;
   EvalInfo ei;
 
   // Probe the material hash table
@@ -777,11 +767,10 @@ Value evaluate(const Pos *pos)
   ei.pe = pawn_probe(pos);
   score += ei.pe->score;
 
-  // We have taken into account all cheap evaluation terms.
-  // If score exceeds a threshold return a lazy evaluation.
-  Value lazy = lazy_eval(mg_value(score), eg_value(score));
-  if (lazy)
-    return pos_stm() == WHITE ? lazy : -lazy;
+  // Early exit if score is high
+  v = (mg_value(score) + eg_value(score)) / 2;
+  if (abs(v) > LazyThreshold)
+     return pos_stm() == WHITE ? v : -v;
   
   // Initialize attack and king safety bitboards.
   evalinfo_init(pos, &ei, WHITE);
@@ -821,8 +810,8 @@ Value evaluate(const Pos *pos)
   // Interpolate between a middlegame and a (scaled by 'sf') endgame score
   //  Value v =  mg_value(score) * ei.me->gamePhase
   //           + eg_value(score) * (PHASE_MIDGAME - ei.me->gamePhase) * sf / SCALE_FACTOR_NORMAL;
-  Value v =  mg_value(score) * ei.me->gamePhase
-           + eg * (PHASE_MIDGAME - ei.me->gamePhase) * sf / SCALE_FACTOR_NORMAL;
+  v =  mg_value(score) * ei.me->gamePhase
+     + eg * (PHASE_MIDGAME - ei.me->gamePhase) * sf / SCALE_FACTOR_NORMAL;
 
   v /= PHASE_MIDGAME;
 
